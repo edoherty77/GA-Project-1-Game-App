c7e14e33ad5da0057f40e507a828554223b021dc
Evan Doherty <edoherty77@gmail.com>
2020-07-15 19:50:14 -0400

    hint functionality created and works most of the time

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
+// Create an array of game puzzles
+const solutionArray = [
+[5, 2, 7, 9, 4, 6, 1, 3, 8],
+[8, 9, 4, 1, 3, 5, 2, 7, 6],
+[1, 6, 3, 7, 2, 8, 9, 4, 5],
+[3, 8, 2, 6, 9, 4, 7, 5, 1],
+[4, 7, 5, 8, 1, 2, 6, 9, 3],
+[9, 1, 6, 3, 5, 7, 4, 8, 2],
+[7, 3, 8, 2, 6, 9, 5, 1, 4],
+[6, 5, 1, 4, 7, 3, 8, 2, 9],
+[2, 4, 9, 5, 8, 1, 3, 6, 7]
+]
+
+
+
 // Variables / Cached Elements
 const footer = document.querySelector("footer")
 const tableBody = document.querySelector("#table-body")
@@ -10,24 +25,17 @@ const mistakeCount = document.querySelector("#mistakeCount")
 const startAndPause = document.querySelector("#start-pause")
 const greeting = document.querySelector("#greeting")
 const pausedMessage = document.querySelector("#paused-message")
+const hintBtn = document.querySelector("#hint")
 
 // Global variables storing values for squares picked and bottom buttons picked
 let answer;
 let squarePicked;
+let time = 30;
+let squareIndex = 1
+let randomIndex;
 
-// Creat solution arrays to check user's answers
 
-const solutionArray = [
-[5, 2, 7, 9, 4, 6, 1, 3, 8],
-[8, 9, 4, 1, 3, 5, 2, 7, 6],
-[1, 6, 3, 7, 2, 8, 9, 4, 5],
-[3, 8, 2, 6, 9, 4, 7, 5, 1],
-[4, 7, 5, 8, 1, 2, 6, 9, 3],
-[9, 1, 6, 3, 5, 7, 4, 8, 2],
-[7, 3, 8, 2, 6, 9, 5, 1, 4],
-[6, 5, 1, 4, 7, 3, 8, 2, 9],
-[2, 4, 9, 5, 8, 1, 3, 6, 7]
-]
+
 
 // Have the start button disabled when the page loads
 startAndPause.disabled = true;
@@ -44,14 +52,16 @@ const start = () => {
 
 		// Call toggle function to toggle elements
 		toggleElements()
+
+		// Call the setTimer function
+		// setTimer()
 	})
-	
 }
+
 
 
 // Show and hide game table and toggle pause and start button
 const toggleElements = () => {
-
 	// Toggle start/pause button and paused message
 	if(startAndPause.innerText === "Start"){
 		startAndPause.innerText = "Pause"
@@ -66,6 +76,7 @@ const toggleElements = () => {
 		gameTable.style.display = "none"
 	} else {
 		gameTable.style.display = "block"
+		
 	}
 	
 }
@@ -80,15 +91,13 @@ const toggleElements = () => {
  		numButton.textContent = i
  		numButton.setAttribute("value", i)
  		
-
  		//Give the buttons some style and room
  		numButton.classList.add("btn", "btn-primary", "numButton")
  		
-
  		//push each button into footer section of html
  		footer.appendChild(numButton)
 
- 		getAnswer(numButton)
+ 		getAnswerValue(numButton)
  	}
  }
 
@@ -96,14 +105,10 @@ const toggleElements = () => {
 
 
  // Set up squares (inputs) for the game board
-
  const createSquares = () => {
-
-
  	//from 0 to 9, create rows
  	for(let i = 0; i < 9; i++){
  		let row = document.createElement("tr")
-
 
  		// From 0 to 9, create columns
  		for(let j = 0; j < 9; j++){
@@ -113,30 +118,34 @@ const toggleElements = () => {
  			let square = document.createElement("button")
  			square.classList.add("btn", "game-squares")
  			
- 			
-
  			 
   			// Set button's value and text to be number from solution array		
  			square.setAttribute("value", solutionArray[i][j])
  			square.innerText = solutionArray[i][j]
-
-
  			
-
 			
  			// Append square divs to each cell, and then each cell to each row
  			cell.appendChild(square)
  			row.appendChild(cell)
 
+ 			
 
 			// Call random number function
- 			randomNumAssignment(square)
+ 			assignRandomNum(square)
 
  			// Call clearByDifficulty function to clear appropriate amount of cell
  			clearByDifficulty(square)
 
  			// Call the getSquareValue function to obtain the value of square selected by user
- 			getSquareValue(square)			
+ 			getSquareValue(square)	
+ 			
+
+ 			assignSquareIndex(square)
+ 			
+ 			// console.log(typeof square.name)
+ 		
+
+ 			getHint(square)		
  		}
  		// Append each row to our table
  		tableBody.appendChild(row)
@@ -147,14 +156,18 @@ const toggleElements = () => {
 
 
 // Create a function to generate a random number. Assign this number to each square, thereby randomly hiding squares for each difficulty
-const randomNumAssignment = (square) => {
+const assignRandomNum = (square) => {
 	const randomNum = Math.floor(Math.random() * 100) + 1
 	square.setAttribute("random", randomNum)
 }
 
 
-
-
+// Create function to assign numbers 1-81 to each square
+const assignSquareIndex = (square) => {
+	square.setAttribute("name", squareIndex)
+	squareIndex++
+	
+}
 
 
 
@@ -163,40 +176,28 @@ const clearByDifficulty = (square) => {
 	easy.addEventListener("click", function(){
 		if(square.getAttribute("random") % 2 == 0){
 			square.innerText = " "
-			square.style.width = "26px"
-			square.style.height = "30px"
 			startAndPause.disabled = false;
 		}
 	})
 	medium.addEventListener("click", function(){
 		if(square.getAttribute("random") % 2 == 0 || square.getAttribute("random")% 4 == 0){
 			square.innerText = " "
-			square.style.width = "26px"
-			square.style.height = "30px"
 			startAndPause.disabled = false;
 		}
 	})
 	hard.addEventListener("click", function(){
 		if(square.getAttribute("random") % 2 == 0 || square.getAttribute("random") % 3 == 0){
 			square.innerText = " "
-			square.style.width = "26px"
-			square.style.height = "30px"
 			startAndPause.disabled = false;
 		}
 	})
 	expert.addEventListener("click", function(){
 		if(square.getAttribute("random") % 2 == 0 || square.getAttribute("random") % 3 == 0 || square.getAttribute("random") % 4 == 0){
 			square.innerText = " "
-			square.style.width = "26px"
-			square.style.height = "30px"
 			startAndPause.disabled = false;
 		}
 	})
 }
-
-
-
-
 
 
 
@@ -211,8 +212,7 @@ const getSquareValue = (square) => {
 
 
 // Get answer chosen from the user
-const getAnswer = (numButton) => {
-
+const getAnswerValue = (numButton) => {
 	// Set the global variable "answer" to the value of the bottom number chosen
 	numButton.addEventListener("click", function(){
 
@@ -228,6 +228,7 @@ const getAnswer = (numButton) => {
 	})
 }
 
+
 // Create function to keep track of user mistakes
 const markMistakes = () => {
 	num = 1
@@ -235,22 +236,79 @@ const markMistakes = () => {
 		mistakeCount.innerText = num
 		num++
 	} 
-	
+}
+
+
+///////////////////////////////////////////////
+//CODE FOR HINT BUTTON FUNCTIONALITY
+///////////////////////////////////////////////
+
+// Create function to come up with a random number 1-81
+const generateRandomIndex = () => {
+	randomIndex = Math.floor(Math.random() * 81) + 1
+}
+
+// Create function to find a square thats empty, and then fill it with it's value
+const checkSquareEmptiness = (square) => {
+	// Find the square whose name = random number
+	if(parseInt(square.name) == randomIndex){
+		let hintSquare = square
+
+		// If square is empty
+			
+		if(hintSquare.innerHTML == " "){
+			// make the square.innerHTML = square.value
+			square.innerHTML = square.value
+			square.style.color = 'rgb(3, 252, 182)'
+			console.log(square)
+		} else {
+			// If that square is not empty epeat the function
+			console.log(square)
+			generateRandomIndex()
+			console.log(randomIndex)
+			checkSquareEmptiness(square)
+		}
+	} 
+}
+
+
+// Create function for hintBtn click event to run above code
+const getHint = (square) => {
+	// Create event listener on the hint button
+	hintBtn.addEventListener("click", function(){
+		
+		checkSquareEmptiness(square)	
+		
+		// Disable button
+		hintBtn.disabled = true;
+		hintBtn.style.background = "gray"
+	})	
 }
 
 
 
+// const setTimer = () => {
+// 	const timer = setInterval(() => {
+// 		time--;
+// 		// if(startAndPause.innerText = "Start"){
+// 		// 	clearInterval(timer)
+// 		// }
+// 		if(time === 0){
+// 			clearInterval(timer)
+// 			alert("game over dickhead")
+// 		}
+// 		document.querySelector("#timer").innerHTML = ("Time: " + time)
+// 	}, 1000)
+// }
+
+
 createNumButtons()
 createSquares()
+generateRandomIndex()
 start()
 
 
-
-
-
-
-
-
+console.log(randomIndex)
 
 
 
